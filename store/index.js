f054d60364d24270b08834b4c36eee3c2456fe7f
Annshuk <annshuks@gmail.com>
2021-12-21 21:10:24 +0530

    1 file modified

--- store/index.js
+++ store/index.js
@@ -1,5 +1,7 @@
 import { applyMiddleware, createStore, compose } from 'redux';
 import rootReducer from '../reducers';
 import thunk from 'redux-thunk';
-const store = createStore(rootReducer, applyMiddleware(thunk))
-export default store; 
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export default store;
